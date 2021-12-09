9096aa15f99aeaf9a95a3eea349e781e9088f386
Andrej Rypo <xrypak@gmail.com>
2021-12-09 16:33:42 +0100

    tests strict + reformat

--- tests/index.php
+++ tests/index.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is a part of dakujem/time package.
  * @author Andrej Rypak (dakujem) <xrypak@gmail.com>
@@ -8,10 +10,10 @@
 $dir = '.';
 
 // run the test
-$time1 = microtime(TRUE);
+$time1 = microtime(true);
 require_once($dir . '/helper.phpt');
-print '<hr/><pre>helper.phpt | Finished at: ' . date('Y-m-d H:i:s') . ' | Runtime: ' . (microtime(TRUE) - $time1 ) . 's</pre>';
+print '<hr/><pre>helper.phpt | Finished at: ' . date('Y-m-d H:i:s') . ' | Runtime: ' . (microtime(true) - $time1) . 's</pre>';
 
-$time2 = microtime(TRUE);
+$time2 = microtime(true);
 require_once($dir . '/time.phpt');
-print '<hr/><pre>time.phpt | Finished at: ' . date('Y-m-d H:i:s') . ' | Runtime: ' . (microtime(TRUE) - $time2 ) . 's</pre>';
+print '<hr/><pre>time.phpt | Finished at: ' . date('Y-m-d H:i:s') . ' | Runtime: ' . (microtime(true) - $time2) . 's</pre>';
